0adbf3f32973637d407f4865eb5ea81026e9f2e7
htslearn123 <webecam784@acuxi.com>
2024-06-05 10:34:42 +0530

    adding math.js

--- math.js
+++ math.js
@@ -1,15 +1,7 @@
-// console.log(Math.random());
+// // console.log(Math.random());
 
 
-let twoDimensional = [
-    [
-    [1,2],
+// let twoDimensional = "himanshu"
 
-                       [4,5]
-],[
-    [1,2],
-    [4,5]
-]
-]
+// console.log(Array.isArray(twoDimensional))
 
-console.log(twoDimensional)
