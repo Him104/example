b1eeca29eae2b0fb593bcad7589210b518a3a204
himanshu <himanshuchauhan696@gmail.com>
2024-06-17 21:38:36 +0530

    adding media query and styles

--- example.js
+++ example.js
@@ -1,5 +1,23 @@
-function add(a,b) {
-return a+b;    
-}
 
-console.log(add(2,2));
+
+
+
+// console.log(a)
+// {const a = 10;
+// console.log(a)
+// }
+
+// console.log(typeof a);
+
+const obj = {name:'him', age:30,location:{city:'Noida'}}
+const shallowCopy = Object.assign({}, obj);
+
+console.log(shallowCopy)
+
+shallowCopy.name = 'saket';
+
+console.log(obj.name);
+
+shallowCopy.location.city = "Nowhere"
+
+console.log(obj.location.city)
