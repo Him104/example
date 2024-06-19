069a7c49962bd2bba0504a5883bc3cf016037607
himanshu <himanshuchauhan696@gmail.com>
2024-06-19 21:49:33 +0530

    adding var and let

---
+++ random.js
@@ -0,0 +1,37 @@
+var name; // variable declaration
+
+name = 'himanshu' // initialisation
+
+
+
+// var isStudent = false;
+
+// console.log(typeof isStudent,  typeof age, typeof name);
+
+// var counter = 'I am a dev';
+
+// function psy() {
+
+// console.log(counter)    
+// }
+
+// psy();
+
+// console.log(counter)
+{
+
+let foo = "I am a block scoped";
+
+// console.log(foo)
+
+foo = "I have been replaced"
+// console.log(foo)
+
+foo = 20;
+
+// console.log(foo)
+
+
+}
+
+console.log(foo)
---
+++ shallow.tsx
@@ -0,0 +1,35 @@
+// let Originalarr = [1,2,[3,4]];
+// let shallowCopy = [...Originalarr]
+
+// shallowCopy[2][0] = 99;26}`;
+
+
+// console.log('Original Array', Originalarr);
+// console.log('Shallow copy Array', shallowCopy);
+
+
+// let Originalarr = [1,2,[3,4]];
+// let deepCopy = JSON.parse(JSON.stringify(Originalarr))
+
+// deepCopy[2][0] = 99;
+
+
+// console.log('Original Array', Originalarr);
+// console.log('Shallow copy Array', deepCopy);
+
+
+// let saket = {"name":"sooraj","age":20};
+//  let doingParse = JSON.stringify(saket)
+
+//  console.log(doingParse)
+
+const user = {
+    firstName: "Angela",
+    lastName: "Davis",
+    role: "Professor",
+  }
+   
+  console.log(user.firstName)
+  
+
+
