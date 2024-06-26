2e81492e0b7626b1dc85ded04b6eeabe1a77d1c4
himanshu <himanshuchauhan696@gmail.com>
2024-06-26 12:06:50 +0530

    adding coercion

--- arr.js
+++ arr.js
@@ -16,18 +16,72 @@
 // const slicedText = text.slice(-7)
 
 // console.log(slicedText)
+// const arr = [1,2,3,4,5,5,6]
+
+// const unknownVariable = arr.map(counter)
+
+// function counter(num){
+//     return num * 20;
+// }
+
+// console.log(unknownVariable)
+// let testingMap = new Map();
+
+// testingMap.set('name','anonymous')
+// testingMap.set('age',27)
+
+// console.log(testingMap.get('name'))
+// console.log(testingMap.get('age'))
+
+// testingMap.delete('age')
+
+// console.log(testingMap.has('age'))
+
+// console.log(testingMap.size)
+
+// function name(a,b) { // parameters
+
+//     return a+b;
+    
+// }
+
+// console.log(name(2,2)) // arguments
 
 
-let testingMap = new Map();
+// var no = null;
+// var non = 'khdkhskh'
 
-testingMap.set('name','anonymous')
-testingMap.set('age',27)
+// console.log(typeof no)
 
-console.log(testingMap.get('name'))
-console.log(testingMap.get('age'))
+// let num = 123
 
-testingMap.delete('age')
+// let str = String(num)
 
-console.log(testingMap.has('age'))
 
-console.log(testingMap.size)
+// console.log(str)
+
+// let str = "23465"
+
+// let num = Number(str)
+
+
+// console.log(typeof num)
+
+
+// let value = 0
+
+// let bool = Boolean(value)
+
+// console.log(typeof bool)
+
+// let num = 9;
+// let str = "9"
+
+
+// console.log(typeof str)
+// console.log(typeof num)
+// console.log(num === str)
+
+var jhdjd = 8728282
+var kdjkd = "huabdhdhj"
+ 
