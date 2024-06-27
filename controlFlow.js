bf39b4d5751fb5f59a881d8a78b77d4be66c0a9d
himanshu <himanshuchauhan696@gmail.com>
2024-06-27 12:11:10 +0530

    controlFolow and switch

---
+++ controlFlow.js
@@ -0,0 +1,15 @@
+let man = 18;
+
+if (man < 18) {
+
+    console.log("He is a student");
+    
+}
+else if (man > 17) {
+
+    console.log("He is a college graduate now...")
+    
+}
+else {
+    console.log("Neither he is less than or greater 18 years of age..")
+}
---
+++ switch.js
@@ -0,0 +1,31 @@
+let day = 5;
+let dayName;
+
+switch (day) {
+  case 1: 
+  dayName =  "Monday"
+    break;
+
+  case 2: 
+  dayName = "Tuesday"
+    break;
+
+  case 3: 
+  dayName = "Wednesday"
+    break;
+
+  case 4 : 
+  dayName = "Thursday"
+    break;
+
+  case 5 : 
+  dayName = "Friday"
+
+    break;
+
+  default:
+dayName = "didn't match any day"
+break;
+}
+
+console.log("the day is : " + dayName)
