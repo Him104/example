a3b07a4a1013df135a5f2dd9bea51975f49d05b1
himanshu <himanshuchauhan696@gmail.com>
2024-07-02 11:53:43 +0530

    adding ternary

---
+++ ternary.js
@@ -0,0 +1,54 @@
+// let age = 101;
+
+// let xyz = (age % 2 === 0) ? 'Even' : 'Odd';
+
+// console.log(xyz)
+
+// let age = 100
+
+// let count = (age%2)
+
+// console.log(count)
+
+
+// function abc() {
+// var xyz = "I am local scoped"
+// console.log(xyz)
+// }
+
+// abc()
+
+// function hello(name) { //Function declaration
+
+//     return `Hello,${name}!`;
+    
+// }
+
+// console.log(hello("Himanshu")) //Function Invocation
+
+
+// const Name = "Deepak"
+// const age = 28;
+
+// console.log(`My name is ${Name} and my age is ${age}`)
+
+
+// let anyVariable = 'I am a Global scoped'
+
+// function printingSomething() {
+
+//     // console.log(anyVariable)
+    
+// }
+
+// console.log(anyVariable)
+
+// printingSomething()
+
+function add(a,b) {
+
+    return a + b;
+    
+}
+
+console.log(add(2 , 4))
