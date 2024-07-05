926f419aa6fc5c8436da92f70af7ddd9bef92d3a
himanshu <himanshuchauhan696@gmail.com>
2024-07-05 11:41:36 +0530

    adding es6

--- ternary.js
+++ ternary.js
@@ -45,10 +45,43 @@
 
 // printingSomething()
 
-function add(a,b) {
 
-    return a + b;
+// var abc = 'xyz';  es5
+//  abc = 123
+
+// console.log(abc)
+
+// let abc = 'xyz'; 
+// abc = 123;
+
+// console.log(abc)
+
+// function add(a,b) {  // function declaration
+
+//     return a + b;
     
+// }
+
+// console.log(add(4 , 4)) // arguments
+
+
+
+// let add = (a,b) => a+b //es6
+
+// console.log(add(2,8))
+
+// let add = (a,b) =>{
+//   return  a+b
+// }  //es6
+
+// console.log(add(2,8))
+
+
+const abc = (fname = "Himanshu") =>{
+    return fname
 }
 
-console.log(add(2 , 4))
+console.log(abc())
+
+
+
