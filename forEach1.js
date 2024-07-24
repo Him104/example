112fd934726673181fdef78be9991a87b8d4a7ca
himanshu <himanshuchauhan696@gmail.com>
2024-07-24 16:41:13 +0530

    adding find, every , some, forEach

---
+++ forEach1.js
@@ -0,0 +1,40 @@
+// const numbers = [1,2,3,4,5,6,7,8]
+
+// numbers.forEach(x => {
+
+// console.log(typeof(x / 2))
+
+// }
+//  )
+
+// const div = numbers.map((element)=>
+// element /2 )
+
+// console.log(typeof div)
+
+// const numbers = [1,2,3,4,5,6,7,8]
+
+
+// const found = numbers.find(x => x > 3)
+
+
+// console.log(found)
+
+
+// const characters = ["cat", "dog", "caterpillar", "snake", "leopard", ""]
+
+// const variableStartswithS = characters.find((characters) => 
+// characters.length===7);
+
+// console.log(variableStartswithS)
+
+const numbers = [1,,3,,5,,7,4]
+
+// const allPositive = numbers.every(x => x> 0)
+// console.log(allPositive)
+
+
+const hasEven = numbers.some(x => x % 2 ===0)
+
+
+console.log(hasEven)
